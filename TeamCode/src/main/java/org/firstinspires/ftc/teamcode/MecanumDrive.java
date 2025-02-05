32128fd326ffc224597603141e58e05048295bfb
ctsmith6828 <christopher@sixbyte.com>
2025-02-05 09:25:47 -0600

    Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,7 +73,7 @@ public final class MecanumDrive {
         public double kA = 0.000095;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 60;
+        public double maxWheelVel = 100;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 50;
 
